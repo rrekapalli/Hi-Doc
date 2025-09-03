60db71ee72fb85494c14fd02c88258bc54dc31df
rrekapalli <rekapallichaitanya@gmail.com>
2025-09-03 15:52:06 +0530

    some issues fixed

--- lib/models/report.g.dart
+++ lib/models/report.g.dart
@@ -7,9 +7,9 @@ part of 'report.dart';
 // **************************************************************************
 
 Report _$ReportFromJson(Map<String, dynamic> json) => Report(
-  id: json['id'] as String,
-  userId: json['user_id'] as String,
-  profileId: json['profile_id'] as String?,
+      id: json['id'] as String,
+      userId: json['user_id'] as String,
+      profileId: json['profile_id'] as String?,
       filePath: json['file_path'] as String,
       fileType: $enumDecode(_$ReportFileTypeEnumMap, json['file_type']),
       source: $enumDecode(_$ReportSourceEnumMap, json['source']),
@@ -22,7 +22,7 @@ Report _$ReportFromJson(Map<String, dynamic> json) => Report(
 Map<String, dynamic> _$ReportToJson(Report instance) => <String, dynamic>{
       'id': instance.id,
       'user_id': instance.userId,
-  'profile_id': instance.profileId,
+      'profile_id': instance.profileId,
       'file_path': instance.filePath,
       'file_type': _$ReportFileTypeEnumMap[instance.fileType]!,
       'source': _$ReportSourceEnumMap[instance.source]!,
